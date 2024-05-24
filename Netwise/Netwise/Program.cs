8064b9f95a325bd1dc8704a928f3304717804885
Koorbik <mandzio80@gmail.com>
2024-05-24 02:10:42 +0200

    Initial working program, which is saving file in the bin folder (to be changed later)

---
+++ Netwise/Netwise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Netwise
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                var response = await httpClient.GetStringAsync("https://catfact.ninja/fact");
+
+                var catFact = response.Trim();
+                var filePath = "cat_facts.txt";
+                File.AppendAllText(filePath, catFact + Environment.NewLine);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
+
