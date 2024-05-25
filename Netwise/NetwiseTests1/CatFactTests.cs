bd042a16086510892e94ce30c76adf3898914f45
Koorbik <mandzio80@gmail.com>
2024-05-26 01:48:51 +0200

    Created unit tests for the app

---
+++ Netwise/NetwiseTests1/CatFactTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Netwise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netwise.Tests
+{
+    [TestClass]
+    public class CatFactTests
+    {
+        [TestMethod]
+        public async Task GetCatFactAsyncTest()
+        {
+            var catFactFetcher = new CatFactFetcher();
+            CatFact? catFact = null;
+ 
+            var catResponse = await catFactFetcher.GetCatFactAsync(catFact);
+            Console.WriteLine(catResponse?.ToString());
+        }
+
+        [TestMethod]
+        public void AppendToFileTest()
+        {
+            var catFactWriter = new CatFactWriter();
+            var catFact = new CatFact { Fact = "Cats are cool.", Length = 13 };
+            var expectedContent = catFact.ToString();
+
+            catFactWriter.AppendToFile(catFact);
+
+            var fileContent = File.ReadAllText("catfact.txt");
+            Assert.IsTrue(fileContent.Contains(expectedContent));
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (File.Exists("catfact.txt"))
+            {
+                File.Delete("catfact.txt");
+            }
+        }
+    }
+}
